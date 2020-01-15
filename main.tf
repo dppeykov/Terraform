bfcb346fb191e70366a1f17a8b40f8e0c64351ab
Damyan Peykov <dppeykov@gmail.com>
2020-01-15 13:59:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,24 @@ provider "aws" {
 # [CONFIG ...]
 # }
 
+resource "aws_instance" "example" {
+ami = "ami-0c55b159cbfafe1f0"
+instance_type = "t2.micro"
+
+tags = {
+    Name = "terraform-example" # EC2 name
+}
+
+}
+
+# ami = required parameter - Amazon Machine Image (AMI) to run the EC2 instance - can find free/paid on AWS marketplace or build with Packer
+# instance_type = required parameter - https://aws.amazon.com/ec2/instance-types/ - t2.micro = free tier
+# terraform documentation - https://www.terraform.io/docs/providers/aws/r/instance.html
+
+# To run the main.tf file go to the same folder in the terminal and type: terraform init
+# The initial binary doesn't have the code for the providers, so on the first run it will scan the code and download what is needed
+# terraform init (idempotent command - can run multiple times with the same result) -> terraform plan (to see what will be deployed before actually do it)
+# terraform plan: (+) - be created, (–) - be deleted, (~) - be modified in place
+# terraform apply - to actually create the infrastructure - asks for a confirmation to apply - keeps track of what's already created from that file
+
+# Good to exclude from version control = .gitignore - .terraform (folder for temp scratch files), *.tfstate (terraform state files) and *.tfstate.backup
